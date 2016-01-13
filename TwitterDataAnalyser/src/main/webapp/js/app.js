33d1129444e7afaf03ce84dd030c31bf928a87a6
Vishal <vishal.a.bhalla@gmail.com>
2016-01-13 09:13:22 +0100

    Precise Personality Type Description with only best adjectives.

--- TwitterDataAnalyser/src/main/webapp/js/app.js
+++ TwitterDataAnalyser/src/main/webapp/js/app.js
@@ -10,20 +10,20 @@ app.controller('mainController', function($scope, $http) {
 	});
 	$scope.personalityTypes = [ {
 		name : 'Analyst',
-		desc : ' Analysts embrace rationality and impartiality, excelling in intellectual debates. They are fiercely independent, open-minded, strong-willed and imaginative, approaching many things from a utilitarian perspective and being far more interested in what works than what satisfies everybody. These traits make Analysts excellent strategic thinkers, but also cause difficulties when it comes to social or romantic pursuits.',
+		desc : ' Analysts are rational and impartial. They are fiercely independent, open-minded, strong-willed and imaginative. These traits make Analysts excellent strategic thinkers, but also cause difficulties when it comes to social or romantic pursuits.',
 		selected : false
 
 	}, {
 		name : 'Diplomat',
-		desc : ' Diplomats focus on empathy and cooperation, shining in counselling. They are cooperative and imaginative, often playing the role of harmonizers in their workplace or social circles. These traits make Diplomats warm, empathic and influential individuals, but also cause issues when there is a need to rely exclusively on cold rationality or make difficult decisions.',
+		desc : ' Diplomats focus on empathy and cooperation. They are imaginative, being harmonizers in their workplace or social circles. These traits make Diplomats warm, empathic and influential individuals, but also cause issues when there is a need to rely exclusively on cold rationality or make difficult decisions.',
 		selected : false
 	}, {
 		name : 'Sentinel',
-		desc : ' Sentinels are cooperative and highly practical, embracing and creating order, security and stability wherever they go. They tend to be hard working, meticulous and traditional. They stick to their plans and do not shy away from difficult tasks. However, they can also be very inflexible and reluctant to accept different points of view.',
+		desc : ' Sentinels are hard working, meticulous and traditional and create order, security and stability wherever they go. They stick to their plans and do not shy away from difficult tasks. However, they can also be very inflexible and reluctant to accept different points of view.',
 		selected : false
 	}, {
 		name : 'Explorer',
-		desc : ' Explorers are utilitarian and practical, spontaneous and shining in situations that require quick reaction and ability to think on your feet. They are masters of tools and techniques, using them in many different ways – ranging from mastering physical tools to convincing other people. They share the ability to connect with their surroundings & their traits can also push them towards undertaking risky endeavors or focusing solely on sensual pleasures.',
+		desc : ' Explorers are utilitarian and practical, spontaneous and shining in situations that require quick reaction and ability to think on your feet. They are masters in physical tools and techniques for convincing other people. They share the ability to connect with their surroundings & undertake risky endeavors or focus solely on sensual pleasures.',
 		selected : false
 	} ];
 	$scope.selection = {
